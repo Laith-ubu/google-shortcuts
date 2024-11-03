93882a857883e7ce7277152a321301eb61708b44
chain <ballout@MacBook-Pro.local>
2024-11-03 16:27:33 +0300

    google shortcuts code

---
+++ resources/views/index.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=apps" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <div class="navBar">
+        <div class="right">
+            <div class=" margn"><a href="">Gmail</a></div>
+            <div class=" margn"><a href="">Images</a></div>
+            <div class=" margn"><a><span class="material-symbols-outlined">apps</span></a></div>
+            <div class="paddin"><a href=""><img src="https://lh3.googleusercontent.com/-8PKX2g4fBGE/AAAAAAAAAAI/AAAAAAAAAAA/ALKGfknK2nfAKId5GSXZJ4XPn8O6YyGwoQ/photo.jpg?sz=46" class="profile-navBar" alt=""></a></div>
+        </div>
+    </div>
+        <div class="middle">
+            <div class="middle-name">
+                <h1 class="font-size-h1">Google</h1>
+            </div>
+        <div class="mid-div-search">
+            <form action="">
+            <input class="search paddin2" type="text" placeholder="Search Google or type a URL.." name="search">
+            <button><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+        <div class="adj-shorcuts">
+            <div class="paddin2">
+                <a class="center-middle" href="">
+                    <div class="gray-backg">
+                        <div class="adjust">
+                            <img class="pic-middle" src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png" alt="">
+                        </div>
+                    </div>
+                    <h5 class="font-size-h5">Youtube</h5>
+                </a>
+            </div>
+            <div class="paddin2">
+                <a class="center-middle" href="">
+                    <div class="gray-backg">
+                        <div class="adjust">
+                            <img class="pic-middle" src="https://thumbs.dreamstime.com/b/plus-add-flat-icon-cross-round-simple-button-circular-vector-sign-style-design-95341045.jpg" alt="">
+                        </div>
+                    </div>
+                    <h5 class="font-size-h5">Add Shortcut</h5>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="location">
+        <div class="locat paddin2">
+            <h5 class="font-size-h5">Jordan</h5>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-right paddin">    
+            <div class=" margn"><a href="">About</a></div>
+            <div class=" margn"><a href="">Advertising</a></div>
+            <div class=" margn"><a href="">Business</a></div>
+            <div class=" margn"><a href="">How Search Works</a></div>
+        </div>
+
+        <div class="footer-left paddin">
+            <div class=" margn"><a href="">Privacy</a></div>
+            <div class=" margn"><a href="">Terms</a></div>
+            <div class=" margn"><a href="">Settings</a></div>
+        </div>
+
+    </footer>
+</body>
+</html>
